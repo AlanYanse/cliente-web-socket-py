1a161b3ebf57338246671b82063c00033b15ec9d
AlanYanse <yansealan@gmail.com>
2024-10-06 14:21:52 -0300

    comenzando a desarrollar un chat

--- cliente.py
+++ cliente.py
@@ -6,9 +6,21 @@ import websockets
 async def communicate():
     uri = "ws://childlike-flicker-hubcap.glitch.me/socket"
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Hola desde Python! ... funciona")
-        response = await websocket.recv()
-        print(f"Respuesta del servidor: {response}")
+        #await websocket.send("Hola desde Python! ... funciona")
+        #response = await websocket.recv()
+        #print(f"Respuesta del servidor: {response}")
+        print("Conectado al servidor.")
+        
+        # Un bucle para enviar y recibir mensajes continuamente
+        while True:
+            message = input("Tú: ")  # Entrada del usuario
+            if message.lower() == "salir":  # Permitir al usuario salir del chat
+                print("Desconectando...")
+                break
+            await websocket.send(message)
+            
+            response = await websocket.recv()
+            print(f"Servidor: {response}")
 
 # Ejecutar la comunicación
 asyncio.get_event_loop().run_until_complete(communicate())
